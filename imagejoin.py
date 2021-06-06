fb5e31e8f3961d3f9afa2e3ceca3dbb57f91659f
cody <62126553+sxmcody@users.noreply.github.com>
2021-06-06 14:17:17 +0800

    Update imagejoin.py
    由于os.listdir排序和Windows资源管理器排序规则不同，故增加了排序，但仅限于以数字命名文件。

--- imagejoin.py
+++ imagejoin.py
@@ -6,38 +6,43 @@ import numpy as np
 import os
 from PIL import Image
 
-imgdir = input("请输入图片文件夹地址:") #指定图片位置，形如：D:\Pictures
-allfiles = os.listdir(imgdir)
+def main():
+    imgdir = input("请输入图片文件夹地址:") #指定图片位置，形如：D:\Pictures
+    allfiles = os.listdir(imgdir)
 
-#用filter()函数过滤仅保留图片格式文件，留下的进入列表
-def file_filter(f):
-    if f[-4:] in ['.jpg','.png','.bmp']:
-        return True
-    else:
-        return False
+    #用filter()函数过滤仅保留图片格式文件，留下的进入列表
+    def file_filter(f):
+        if f[-4:] in ['.jpg','.png','.bmp']:
+            return True
+        else:
+            return False
 
-images = list(filter(file_filter,allfiles))
+    images = list(filter(file_filter,allfiles))
+    images.sort(key = lambda x:int(x[:-4])) #对图片进行排序
+    new_images = []  #建立一个列表，稍后依次将打开的图片存入其中
 
-new_images = []  #建立一个列表，稍后依次将打开的图片存入其中
-
-resized_images = []  #建立一个新列表，稍后将更改过图片尺寸后的图片存入其中
+    resized_images = []  #建立一个新列表，稍后将更改过图片尺寸后的图片存入其中
 
 
-for image in images:
-    #依次打开图片（输入的路径+图片文件名得到实际图片路径）
-    imgopen = Image.open(imgdir +"\\"+ image)
-    new_images.append(imgopen)  #将图片存入列表
-   
-#通过循环，将列表里的每张图片的宽度尺寸以第一张图片为基准重设
-for im in new_images:
-    if im == new_images[0]:  #第一张图片略过处理
-        pass
-    elif im.size[0]== new_images[0].size[0]:  #增加一个判断，如果图片与第一张图片宽度一样，那么略过重设大小
-        pass
-    else:
-        im = im.resize((new_images[0].size[0],int(new_images[0].size[0]*im.size[1]/im.size[0])),Image.BILINEAR)
-    resized_images.append(np.array(im))  #将更改过尺寸的图片转化为ndarray对象并存入列表
+    for image in images:
+        #依次打开图片（输入的路径+图片文件名得到实际图片路径）
+        imgopen = Image.open(imgdir +"\\"+ image)
+        new_images.append(imgopen)  #将图片存入列表
+       
+    #通过循环，将列表里的每张图片的宽度尺寸以第一张图片为基准重设
+    for im in new_images:
+        if im == new_images[0]:  #第一张图片略过处理
+            pass
+        elif im.size[0]== new_images[0].size[0]:  #增加一个判断，如果图片与第一张图片宽度一样，那么略过重设大小
+            pass
+        else:
+            im = im.resize((new_images[0].size[0],int(new_images[0].size[0]*im.size[1]/im.size[0])),Image.BILINEAR)
+        resized_images.append(np.array(im))  #将更改过尺寸的图片转化为ndarray对象并存入列表
 
-imgjoin = np.concatenate(resized_images,axis = 0)  #拼接图片，axis=0为纵向拼接
-imgcreate = Image.fromarray(imgjoin)  #生成图片
-imgcreate.save('final.jpg')  #保存图片并以final.jpg命名
+    imgjoin = np.concatenate(resized_images,axis = 0)  #拼接图片，axis=0为纵向拼接
+    imgcreate = Image.fromarray(imgjoin)  #生成图片
+    imgcreate.save('final.jpg')  #保存图片并以final.jpg命名
+
+if __name__ == '__main__':
+    main()
+
